ae5752ec02b81fcac5289e510555dc058a90c5ea
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 12:02:15 -0500

    mapped date categories

--- src/components/DateCategory.js
+++ src/components/DateCategory.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const DateCategory = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const categories = [
+    "Tomorrow",
+    "This Week",
+    "This Month",
+    "Calendar",
+  ];
 
-export default DateCategory
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <h3 className="text-center">Today</h3>
+        </div>
+      </div>
+      {categories.map((category) => (
+        <div className="row" key={category} style={{borderTop: 'black 2px solid'}}>
+          <div className="col">
+            <h3 className="text-center">{category}</h3>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default DateCategory;
