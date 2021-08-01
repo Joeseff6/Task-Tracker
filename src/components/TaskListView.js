09b56060bd83a7ba38c46b08009b7dd6dd1c9262
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 12:03:34 -0500

    imported DateCategory component

--- src/components/TaskListView.js
+++ src/components/TaskListView.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "./Button";
+import DateCategory from "./DateCategory";
 
 const TaskListView = () => {
   return (
     <div className="row justify-content-center">
-      <div className="col col-sm-6 taskListView">
+      <div className="col col-sm-9 taskListView">
         <div
           className="row mt-2 pb-2 align-items-center"
           style={{ borderBottom: "black 2px solid" }}
@@ -14,6 +15,14 @@ const TaskListView = () => {
           </div>
           <div className="col">
             <Button text="Add Task" backgroundColor="green" />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-4' style={{borderRight: 'black 2px solid'}}>
+            <DateCategory />
+          </div>
+          <div className='col-8'>
+            {/* Add date categories here */}
           </div>
         </div>
       </div>
