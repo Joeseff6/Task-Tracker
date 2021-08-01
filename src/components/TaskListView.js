6e2cbbf641ac06f0c6a637b773b92819df7eba70
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 12:43:01 -0500

    used useEffect to pull tasks from json-server

--- src/components/TaskListView.js
+++ src/components/TaskListView.js
@@ -1,20 +1,33 @@
-import React from "react";
-import Button from "./Button";
-import DateCategory from "./DateCategory";
+import { useState, useEffect } from 'react';
+import Button from './Button';
+import DateCategory from './DateCategory';
+import axios from 'axios';
+import Tasks from './Tasks';
 
 const TaskListView = () => {
+  const [ tasks, setTasks ] = useState([]);
+
+  useEffect(() => {
+    getTasks();
+  },[])
+
+  const getTasks = async() => {
+    const { data } = await axios.get('http://localhost:5000/tasks');
+    setTasks(data)
+  }
+
   return (
-    <div className="row justify-content-center">
-      <div className="col col-sm-9 taskListView">
+    <div className='row justify-content-center'>
+      <div className='col col-sm-9 taskListView'>
         <div
-          className="row mt-2 pb-2 align-items-center"
-          style={{ borderBottom: "black 2px solid" }}
+          className='row mt-2 pb-2 align-items-center'
+          style={{ borderBottom: 'black 2px solid' }}
         >
-          <div className="col">
-            <h2 className="text-center">My Task List</h2>
+          <div className='col'>
+            <h2 className='text-center'>My Task List</h2>
           </div>
-          <div className="col">
-            <Button text="Add Task" backgroundColor="green" />
+          <div className='col'>
+            <Button text='Add Task' backgroundColor='green' />
           </div>
         </div>
         <div className='row'>
@@ -22,7 +35,7 @@ const TaskListView = () => {
             <DateCategory />
           </div>
           <div className='col-8'>
-            {/* Add date categories here */}
+            <Tasks tasks={tasks} />
           </div>
         </div>
       </div>
