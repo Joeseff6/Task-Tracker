8c968991d4d6406538367d7e41cfeb8e6c5fb225
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 12:43:12 -0500

    mapped out tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
-    <div>
-      
+    <div className='row'>
+      {tasks.map((task) => (
+        <div className="col-4" key={task.id} style={{borderBottom: 'black 2px solid', borderRight: 'black 2px solid'}}>
+          <h6>{task.task}</h6>
+          <span>{task.date}</span>
+          <span>{task.reminder}</span>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
