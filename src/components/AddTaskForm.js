818ce88e596e4a9bb9a73bfc12f4e2ac23c11cad
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 22:38:01 -0500

    Added input fields for adding tasks

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -2,50 +2,54 @@ import { useState } from "react";
 import Button from "./Button";
 
 const AddTaskForm = () => {
+  const [taskForm, setTaskForm] = useState({
+    taskName: "",
+    time: "",
+    date: "",
+    reminder: false,
+  });
+
   return (
     <form>
       <div className="input-group mb-3">
-        <span className="input-group-text">
-          Task name:
-        </span>
+        <span className="input-group-text">Task name:</span>
         <input
           type="text"
           className="form-control"
           placeholder="Do the dishes"
+          onChange={(e) =>
+            setTaskForm({ ...taskForm, taskName: e.target.value })
+          }
+          required
         />
       </div>
       <div className="input-group mb-3">
-        <span className="input-group-text">
-          Time:
-        </span>
+        <span className="input-group-text">Time:</span>
         <input
           type="time"
           className="form-control"
+          onChange={(e) => setTaskForm({ ...taskForm, time: e.target.value })}
+          required
         />
       </div>
       <div className="input-group mb-3">
-        <span className="input-group-text">
-          Date:
-        </span>
+        <span className="input-group-text">Date:</span>
         <input
           type="date"
           className="form-control"
+          onChange={(e) => setTaskForm({ ...taskForm, date: e.target.value })}
+          required
         />
       </div>
-      <div
-        className="btn-group"
-        role="group"
-      >
+      <div>
+        <label htmlFor="reminder" className='me-3 h5'>Reminder?</label>
         <input
           type="checkbox"
-          className="btn-check"
-          id="btncheck1"
+          id="reminder"
+          onChange={(e) => setTaskForm({ ...taskForm, reminder: e.target.checked })}
         />
-        <label className="btn btn-outline-primary" htmlFor="btncheck1">
-          Reminder?
-        </label>
       </div>
-      <Button text='Submit Task' backgroundColor='green' color='black'/>
+      <Button text="Submit Task" backgroundColor="green" color="black" />
     </form>
   );
 };
