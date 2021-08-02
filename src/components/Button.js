4cbf1e0ef37b49f79da1b1ffcd4a0a958cecf011
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 22:23:01 -0500

    removed div class for row

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,12 @@
 const Button = ({ text, backgroundColor, color }) => {
   return (
-    <div>
-      <button className='btn mb-3' style={{backgroundColor: backgroundColor, color: color}}>
-        <span>{text}</span>
-      </button>
-    </div>
-  )
-}
+    <button
+      className="btn mb-3"
+      style={{ backgroundColor: backgroundColor, color: color }}
+    >
+      <span>{text}</span>
+    </button>
+  );
+};
 
-export default Button
+export default Button;
