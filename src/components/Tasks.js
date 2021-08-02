3fbdbc8fe859364dbe5c3da29837f537ae19fbc6
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 22:23:46 -0500

    changed text size of task information

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,10 +4,10 @@ const Tasks = ({ tasks }) => {
   return (
     <div className='row'>
       {tasks.map((task) => (
-        <div className="col-4" key={task.id} style={{borderBottom: 'black 2px solid', borderRight: 'black 2px solid'}}>
-          <h6>{task.task}</h6>
-          <span>{task.date}</span>
-          <span>{task.reminder}</span>
+        <div className="col-3" key={task.id} style={{borderBottom: 'black 2px solid', borderRight: 'black 2px solid'}}>
+          <h5>{task.task}</h5>
+          <h6>{task.date}</h6>
+          <h6>{task.reminder}</h6>
         </div>
       ))}
     </div>
