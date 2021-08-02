0a3c23d7972757306a9d9111b94f2d92dcfe9bb9
Joseph Soria <josephsoria6@gmail.com>
2021-08-01 22:24:25 -0500

    removed div class for row

--- src/components/DateCategory.js
+++ src/components/DateCategory.js
@@ -1,25 +1,13 @@
 import React from "react";
 
 const DateCategory = () => {
-  const categories = [
-    "Tomorrow",
-    "This Week",
-    "This Month",
-    "Calendar",
-  ];
+  const categories = ["Today", "Tomorrow", "This Week", "This Month", "Calendar"];
 
   return (
     <>
-      <div className="row">
-        <div className="col">
-          <h3 className="text-center">Today</h3>
-        </div>
-      </div>
       {categories.map((category) => (
-        <div className="row" key={category} style={{borderTop: 'black 2px solid'}}>
-          <div className="col">
-            <h3 className="text-center">{category}</h3>
-          </div>
+        <div className="col" key={category} style={{borderBottom: '2px solid black'}}>
+          <h3 className="text-center">{category}</h3>
         </div>
       ))}
     </>
