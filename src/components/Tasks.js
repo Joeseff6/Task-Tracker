54c2e83ca7d01d308ec70eba6242075163a31d79
Joseph Soria <josephsoria6@gmail.com>
2021-08-03 21:37:00 -0500

    used the index parameter in .map to give data att

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
+import Button from './Button';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDoubleClick }) => {
   return (
-    <div className='row'>
-      {tasks.map((task) => (
-        <div className="col-3" key={task.id} style={{borderBottom: 'black 2px solid', borderRight: 'black 2px solid'}}>
+    <div className='row taskRow'>
+      {tasks.map((task, index) => (
+        <div className='col-3 task' key={task.id}>
+          <h5>{task.date}</h5>
           <h5>{task.task}</h5>
-          <h6>{task.date}</h6>
-          <h6>{task.reminder}</h6>
+          {task.reminder ? (
+            <Button text='Reminder: On' onDoubleClick={onDoubleClick} index={index}/>
+          ) : (
+            <Button text='Reminder: Off' onDoubleClick={onDoubleClick} index={index}/>
+          )}
         </div>
       ))}
     </div>
