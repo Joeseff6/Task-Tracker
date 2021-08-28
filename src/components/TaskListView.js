4ec55a9165c7e475d561148c3f8bc1e7001f512c
Joseph Soria <josephsoria6@gmail.com>
2021-08-28 18:38:27 -0500

    Commented previous code, made new card

--- src/components/TaskListView.js
+++ src/components/TaskListView.js
@@ -1,73 +1,165 @@
-import { useState, useEffect } from "react";
-import Button from "./Button";
-import DateCategory from "./DateCategory";
-import axios from "axios";
-import Tasks from "./Tasks";
-import AddTaskForm from "./AddTaskForm";
+// import { useState, useEffect } from "react";
+// import Button from "./Button";
+// import DateCategory from "./DateCategory";
+// import axios from "axios";
+// import Tasks from "./Tasks";
+// import AddTaskForm from "./AddTaskForm";
+// const date = new Date();
+// const today = parseInt(date.getDate());
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
 
 const TaskListView = () => {
-  const [tasks, setTasks] = useState([]);
-  const [addTask, setAddTask] = useState(false);
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
-  const getTasks = async () => {
-    const { data } = await axios.get("http://localhost:5000/tasks");
-    setTasks(data);
-  };
-
-  const handleToggleForm = () => {
-    setAddTask(!addTask);
-  };
-
-  const handleToggleReminder = (e) => {
-    const index = e.target.dataset.index;
-    tasks[index].reminder = !tasks[index].reminder;
-    setTasks([...tasks]);
-  }
-  
 
   return (
-    <div className="row justify-content-center">
-      <div className="col col-sm-9 taskListView">
-        <div className="row mt-2 pb-2 align-items-center">
-          <div className="col">
-            <h2 className="text-center">My Task List</h2>
-          </div>
-        </div>
-        <div className="row d-block">
-          <div className="col-4 m-auto">
-            {addTask ? (
-              <Button
-                text="Close Form"
-                backgroundColor="yellow"
-                onClick={handleToggleForm}
-              />
-            ) : (
-              <Button
-                text="Add Tasks"
-                backgroundColor="green"
-                onClick={handleToggleForm}
-              />
-            )}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col" style={{ borderBottom: "black 2px solid" }}>
-            <h3 className="text-center mb-3">Sort by:</h3>
-          </div>
-          <DateCategory />
-        </div>
-        <div className="row">
-          <div className="col">
-            {addTask ? <AddTaskForm /> : <Tasks tasks={tasks} onDoubleClick={handleToggleReminder}/>}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <Row>
+      <Col className="d-flex justify-content-center">
+        <Card className="col-9">
+          <Card.Body className="text-center">
+            <Card.Title className="fs-1 mb-4">Your Task List</Card.Title>
+            <Card.Subtitle className="fs-5">Add, remove, and update tasks!</Card.Subtitle>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
+  // const [tasks, setTasks] = useState([]);
+  // const [addTask, setAddTask] = useState(false);
+
+//   return (
+//     <div className="row justify-content-center">
+//       <div className="col col-sm-9 taskListView">
+//         <div className="row mt-2 pb-2 align-items-center">
+//           <div className="col">
+//             <h2 className="text-center">My Task List</h2>
+//           </div>
+//         </div>
+//         <div className="row d-block">
+//           <div className="col-4 m-auto">
+//             {addTask ? (
+//               <Button
+//                 text="Close Form"
+//                 backgroundColor="yellow"
+//                 onClick={handleToggleForm}
+//               />
+//             ) : (
+//               <Button
+//                 text="Add Tasks"
+//                 backgroundColor="green"
+//                 onClick={handleToggleForm}
+//               />
+//             )}
+//           </div>
+//         </div>
+//         <div className="row">
+//           <div className="col">
+//             {addTask ? (
+//               <AddTaskForm />
+//             ) : tasks.length > 0 ? (
+//               <>
+//                 <div className="row">
+//                   <div
+//                     className="col"
+//                     style={{ borderBottom: "black 2px solid" }}
+//                   >
+//                     <h3 className="text-center mb-3">Sort by:</h3>
+//                   </div>
+//                   <DateCategory onClick={sortFunctions} />
+//                 </div>
+//                 <Tasks tasks={tasks} onDoubleClick={handleToggleReminder} />
+//               </>
+//             ) : (
+//               <h1 className="text-center my-5">No tasks available</h1>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+  // const getTasks = async () => {
+  //   const { data } = await axios.get("http://localhost:5000/tasks");
+  //   console.log(data);
+  //   setTasks(data);
+  // };
+
+  // useEffect(() => {
+  //   getTasks();
+  // }, []);
+
+  // const handleToggleForm = () => {
+  //   setAddTask(!addTask);
+  // };
+
+  // const handleToggleReminder = (e) => {
+  //   const index = e.target.dataset.index;
+  //   tasks[index].reminder = !tasks[index].reminder;
+  //   setTasks([...tasks]);
+  // };
+
+  // const sortByPastDue = async () => {
+  //   await getTasks();
+  //   console.log(tasks);
+  //   const filteredTasks = tasks.filter((task) => {
+  //     const taskDate = parseInt(task.date.split("/")[1]);
+  //     console.log(taskDate < today);
+  //     return taskDate < today;
+  //   });
+  //   setTasks([...filteredTasks]);
+  // };
+
+  // const sortByToday = async () => {
+  //   await getTasks();
+  //   console.log(tasks);
+  //   const filteredTasks = tasks.filter((task) => {
+  //     const taskDate = parseInt(task.date.split("/")[1]);
+  //     return taskDate === today;
+  //   });
+  //   setTasks([...filteredTasks]);
+  // };
+
+  // const sortByTomorrow = () => {
+  //   getTasks();
+  //   const taskDate = parseInt(tasks[0].date.split("/")[1]);
+  //   const filteredTasks = tasks.filter((task) => {
+  //     const taskDate = parseInt(task.date.split("/")[1]);
+  //     return taskDate < today;
+  //   });
+  //   setTasks(filteredTasks);
+  // };
+
+  // const sortByThisWeek = () => {
+  //   getTasks();
+  //   const taskDate = parseInt(tasks[0].date.split("/")[1]);
+  //   const filteredTasks = tasks.filter((task) => {
+  //     const taskDate = parseInt(task.date.split("/")[1]);
+  //     return taskDate < today;
+  //   });
+  //   setTasks(filteredTasks);
+  // };
+
+  // const sortByThisMonth = () => {
+  //   getTasks();
+  //   const taskDate = parseInt(tasks[0].date.split("/")[1]);
+  //   const filteredTasks = tasks.filter((task) => {
+  //     const taskDate = parseInt(task.date.split("/")[1]);
+  //     return taskDate < today;
+  //   });
+  //   setTasks(filteredTasks);
+  // };
+
+  // const sortFunctions = [
+  //   sortByPastDue,
+  //   sortByToday,
+  //   sortByTomorrow,
+  //   sortByThisWeek,
+  //   sortByThisMonth,
+  //   getTasks,
+  // ];
+}
 
 export default TaskListView;
