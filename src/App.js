ba0680d15989601fb9893dd8c507cb59c4e3a8cf
Joseph Soria <josephsoria6@gmail.com>
2021-08-28 16:02:00 -0500

    Updated links for sign up and login views

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
-import SignupForm from "./components/SignupForm";
-import LoginForm from "./components/LoginForm";
+import SignupView from "./components/SignupView";
+import LoginView from "./components/LoginView";
 import TaskListView from "./components/TaskListView";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,15 +11,15 @@ function App() {
   return (
     <Container fluid="md">
       <Header />
-      <TaskListView />
-      {/* <Router>
+      {/* <TaskListView /> */}
+      <Router>
         <Route exact path="/">
-          <LoginForm />
+          {/* <LoginView /> */}
         </Route>
         <Route path="/signup">
-          <SignupForm />
+          <SignupView />
         </Route>
-      </Router> */}
+      </Router>
     </Container>
   );
 }
