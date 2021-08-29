0c80ba9b24d20cf8a021b8dc14a8546796b5fc86
Joseph Soria <josephsoria6@gmail.com>
2021-08-29 16:30:49 -0500

    Removed previous code post re-factor

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -16,24 +16,3 @@ const Tasks = ({ task }) => {
 }
 
 export default Tasks
-
-
-// const Tasks = ({ tasks, onDoubleClick }) => {
-//   return (
-//     <div className='row taskRow'>
-//       {tasks.map((task, index) => (
-//         <div className='col-3 task' key={task.id}>
-//           <h5>{task.date}</h5>
-//           <h5>{task.task}</h5>
-//           <h6> Time: {task.time}</h6>
-//           {task.reminder ? (
-//             <Button text='Reminder: On' onDoubleClick={onDoubleClick} index={index}/>
-//           ) : (
-//             <Button text='Reminder: Off' onDoubleClick={onDoubleClick} index={index}/>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
