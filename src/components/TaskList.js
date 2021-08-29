9ee3fb40e841eafd5982fe6a4b4422b0e8ffa228
Joseph Soria <josephsoria6@gmail.com>
2021-08-29 15:57:09 -0500

    initialized the state object

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+import Tasks from "./Tasks";
+
 
 export default class TaskList extends Component {
+  state = { tasks: [] };
+
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <h1>this is the task list</h1>
   }
 }
+
+
