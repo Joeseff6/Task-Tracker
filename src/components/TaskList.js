4c607200d26ad9b76de7ba23ac5be1f7f70f67a7
Joseph Soria <josephsoria6@gmail.com>
2021-08-29 15:58:42 -0500

    Wrote function to obtain tasks from json-server. Wrote componentDidMount method

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,13 +4,23 @@ import Col from "react-bootstrap/Col";
 import axios from "axios";
 import Tasks from "./Tasks";
 
-
+const fetchTasks = async () => {
+  try {
+    const { data } = await axios.get("http://localhost:5000/tasks");
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 export default class TaskList extends Component {
   state = { tasks: [] };
 
+  async componentDidMount() {
+    const fetchedTasks = await fetchTasks();
+    this.setState({ tasks: fetchedTasks });
+  }
+
   render() {
-    return <h1>this is the task list</h1>
+    return <h1>This is a list</h1>
   }
 }
-
-
