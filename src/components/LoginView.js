d8a0f3547d9d693f1709c277265e7192880bd0d9
Joseph Soria <josephsoria6@gmail.com>
2021-08-29 15:09:48 -0500

    Removed previous code post re-factor

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -21,42 +21,6 @@ const LoginView = () => {
       </Col>
     </Row>
   );
-
-  // return (
-  //   <div className="row justify-content-center">
-  //     <div className="col-sm-6 loginContainer" style={{height: '500px'}}>
-  //       <div className="row">
-  //         <h2 className="text-center mb-3 mt-5">Log in</h2>
-  //         <form>
-  //           <div className="form-floating mb-3 col-9">
-  //             <input
-  //               type="text"
-  //               className="form-control"
-  //               id="floatingUsername"
-  //               placeholder="user123"
-  //               onChange={(e) => setLoginForm({...loginForm, username: e.target.value})}
-  //               required
-  //             />
-  //             <label htmlFor="floatingUsername">Username</label>
-  //           </div>
-  //           <div className="form-floating mb-3 col-9">
-  //             <input
-  //               type="password"
-  //               className="form-control"
-  //               id="floatingPassword"
-  //               placeholder="supersecretpassword"
-  //               onChange={(e) => setLoginForm({...loginForm, password: e.target.value})}
-  //               required
-  //             />
-  //             <label htmlFor="floatingPassword">Password</label>
-  //           </div>
-  //           <Button text="Login" backgroundColor="#43AA8B" />
-  //         </form>
-  //         <Link to='/signup' className='h5'>Not a user yet? Sign up!</Link>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default LoginView;
