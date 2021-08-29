ce22fbbfcb72e65a9225ae83474286d48612a13a
Joseph Soria <josephsoria6@gmail.com>
2021-08-29 15:03:06 -0500

    Converted to Class component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const TaskList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export default class TaskList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default TaskList
