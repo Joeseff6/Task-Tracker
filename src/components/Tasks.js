033eb9f8d7614d056b91a1d557d3e100b4b32c84
Joseph Soria <josephsoria6@gmail.com>
2021-08-30 17:20:51 -0500

    Filled out task card with prop data

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,25 @@
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import { BsCheckCircle, BsXCircle } from "react-icons/bs";
+import Button from "react-bootstrap/Button";
 
 const Tasks = ({ task }) => {
-  console.log(task)
   return (
-    <Col md={5} key={task.id}>
+    <Col md={5}>
       <Card bg="light" className="mb-3">
         <Card.Body>
-          <Card.Title>{task.task}</Card.Title>
-          <Card.Text>{task.date}</Card.Text>
+          <Card.Title>Task: {task.task}</Card.Title>
+          <Card.Text>
+            Date: {task.date} <br />
+            Time: {task.time} <br />
+            <Button variant="light" style={{ border: "2px black solid" }}>
+              Reminder? {task.reminder ? <BsCheckCircle fill="green"/> : <BsXCircle fill="red"/>}
+            </Button>
+          </Card.Text>
         </Card.Body>
       </Card>
     </Col>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
