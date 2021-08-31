8bc21291054774a1b60efff8133b6300d5f95df9
Joseph Soria <josephsoria6@gmail.com>
2021-08-30 22:09:38 -0500

    Moved fetchTasks inside class, updated componentDidMount. Added error conditional rendering

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,19 +4,20 @@ import axios from "axios";
 import Tasks from "./Tasks";
 import Spinner from "react-bootstrap/Spinner";
 
-const fetchTasks = async () => {
-  try {
-    const { data } = await axios.get("http://localhost:5000/tasks");
-    return data;
-  } catch (err) {
-    return [];
-  }
-};
 export default class TaskList extends Component {
   state = { tasks: ["Loading"] };
 
+  async fetchTasks() {
+    try {
+      const { data } = await axios.get("http://localhost:5000/tasks");
+      return data;
+    } catch (err) {
+      return ["Error"];
+    }
+  }
+
   async componentDidMount() {
-    const fetchedTasks = await fetchTasks();
+    const fetchedTasks = await this.fetchTasks();
     this.setState({ tasks: fetchedTasks });
   }
 
@@ -28,14 +29,24 @@ export default class TaskList extends Component {
           <span className="fs-3">Fetching tasks...</span>
         </Row>
       );
-    } else if (this.state.tasks.length > 0) {
-      return this.state.tasks.map((task) => (
-        <Tasks task={task} key={task.id} />
-      ));
-    } else {
+    }
+
+    if (this.state.tasks[0] === "Error") {
       return (
         <Row className="text-center fs-3">
-          <span>No tasks available.</span>
+          <span className="fs-3">Error, couldn't reach the server</span>
+        </Row>
+      );
+    }
+
+    if (this.state.tasks.length > 0) {
+      return this.state.tasks.map((task) => <Tasks task={task} key={task.id}/>);
+    }
+
+    if (this.state.tasks.length === 0) {
+      return (
+        <Row className="text-center fs-3">
+          <span className="fs-3">No tasks available.</span>
         </Row>
       );
     }
