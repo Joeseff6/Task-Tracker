95d2a023fb4adcdeecbd4525a72dfb5ea7a29134
Joseph Soria <josephsoria6@gmail.com>
2021-09-03 23:00:13 -0500

    Wrote onButtonClick method, passed as callback function on Button component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,37 @@
+import React from "react";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import { BsCheckCircle, BsXCircle } from "react-icons/bs";
 import Button from "react-bootstrap/Button";
 
-const Tasks = ({ task }) => {
-  return (
-    <Col md={5}>
-      <Card bg="light" className="mb-3">
-        <Card.Body>
-          <Card.Title>Task: {task.task}</Card.Title>
-          <Card.Text>
-            Date: {task.date} <br />
-            Time: {task.time} <br />
-            <Button variant="light" style={{ border: "2px black solid" }}>
-              Reminder? {task.reminder ? <BsCheckCircle fill="green"/> : <BsXCircle fill="red"/>}
-            </Button>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
-};
+class Tasks extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+  }
+
+  toggleReminder = () => {
+    this.props.onToggle(this.props.index)
+  }
+
+  render() {
+    return (
+      <Col md={5}>
+        <Card bg="light" className="mb-3">
+          <Card.Body>
+            <Card.Title>Task: {this.props.task.task}</Card.Title>
+            <Card.Text>
+              Date: {this.props.task.date} <br />
+              Time: {this.props.task.time} <br />
+              <Button variant="light" style={{ border: "2px black solid"}} onClick={this.toggleReminder}>
+                Reminder? {this.props.task.reminder ? <BsCheckCircle fill="green"/> : <BsXCircle fill="red"/>}
+              </Button>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+}
 
 export default Tasks;
