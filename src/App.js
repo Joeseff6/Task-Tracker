50d6710c1b287b171ed997341804d98f7a84dfe5
Joseph Soria <josephsoria6@gmail.com>
2022-01-16 12:58:45 -0600

    Refactored App to remove unused components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
-import SignupView from "./components/SignupView";
-import LoginView from "./components/LoginView";
 import TaskListView from "./components/TaskListView";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
-function App() {
-  return (
-    <Container fluid="md">
-      <Header />
-      {/* <TaskListView /> */}
-      <Router>
-        <Route exact path="/">
-          {/* <LoginView /> */}
-        </Route>
-        <Route path="/signup">
-          <SignupView />
-        </Route>
-      </Router>
-    </Container>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container fluid="md">
+        <Header />
+        <TaskListView />
+      </Container>
+    );
+  }
 }
 
 export default App;
