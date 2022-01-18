d36497bf153861e0030618ebcc9639422436ff3c
Joseph Soria <josephsoria6@gmail.com>
2022-01-17 22:18:15 -0600

    Changed state to prop

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,23 +1,11 @@
 import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
-import axios from "axios";
 import Tasks from "./Tasks";
 import Spinner from "react-bootstrap/Spinner";
 
 export default class TaskList extends Component {
-    state = { tasks: ["Loading"] };
-
-  async fetchTasks() {
-    try {
-      const { data } = await axios.get("http://localhost:5000/tasks");
-      return data;
-    } catch (err) {
-      return ["Error"];
-    }
-  }
-
   renderJSX() {
-    if (this.state.tasks[0] === "Loading") {
+    if (this.props.tasks[0] === "Loading") {
       return (
         <Row className="justify-content-center">
           <Spinner animation="border" role="status" />
@@ -25,30 +13,30 @@ export default class TaskList extends Component {
         </Row>
       );
     }
-    if (this.state.tasks[0] === "Error") {
+    if (this.props.tasks[0] === "Error") {
       return (
         <Row className="text-center fs-3">
           <span className="fs-3">Error, couldn't reach the server</span>
         </Row>
       );
     }
-    if (this.state.tasks.length) {
-      return this.state.tasks.map((task, index) => (
-        <Tasks task={task} key={task.id} index={index} onToggle={this.onToggle}/>
+    if (this.props.tasks.length) {
+      return this.props.tasks.map((task, index) => (
+        <Tasks
+          task={task}
+          key={task.id}
+          index={index}
+          onToggle={this.onToggle}
+        />
       ));
     }
-    if (!this.state.tasks.length) {
+    if (!this.props.tasks.length) {
       return (
         <Row className="text-center fs-3">
           <span className="fs-3">No tasks available.</span>
         </Row>
       );
     }
-  }
-
-  async componentDidMount() {
-    const fetchedTasks = await this.fetchTasks();
-    this.setState({ tasks: fetchedTasks });
   }
 
   render() {
