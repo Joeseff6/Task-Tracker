ab9f111b5956c05d9e9d64e0a941f4abaa749fa5
Joseph Soria <josephsoria6@gmail.com>
2022-01-26 00:42:13 -0600

    Wrote function to update task in DB when checkbox is checked

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,21 +1,36 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
+import { db } from "../db/db";
+import "./Tasks.css";
 
 class Tasks extends React.Component {
+  onCheckboxClick = (e) => {
+    db.tasks.update(this.props.task.id, {complete: e.target.checked});
+  }
+
   render() {
     return (
-      <Col md={8}>
-        <Card bg="light">
+      <Row className="taskItem">
+        <Card
+          className={this.props.task.complete ? "bg-light completedTask" : "bg-light"}
+        >
           <Card.Body>
             <Card.Title>{this.props.task.task}</Card.Title>
             <Card.Text>
               Date: {this.props.task.date} <br />
               Time: {this.props.task.time} <br />
+              Completed?
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={this.onCheckboxClick}
+              />
             </Card.Text>
           </Card.Body>
         </Card>
-      </Col>
+      </Row>
     );
   }
 }
