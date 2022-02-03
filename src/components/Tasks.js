82955d2501b7b96a0d6c94c66a07eb8df54ee9e8
Joseph Soria <josephsoria6@gmail.com>
2022-02-02 23:11:43 -0600

    Removed unused imports

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import { db } from "../db/db";
 import "./Tasks.css";
